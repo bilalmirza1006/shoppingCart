ab8be23abe79498a382f8cb13cf17d585037e898
Muhammad Bilal Mirza <bilal.tech1006@gmail.com>
2023-10-17 13:35:42 +0500

    add filters

--- Script.js
+++ Script.js
@@ -46,12 +46,12 @@ const product = [
         ratingStar: 2
 
     },
-];
-// const categories = product.map((item) => item.title);
+]
+// const categories = product.map((item) => item.title)
 
-let i = 0;
+let i = 0
 document.getElementById('root').innerHTML = product.map((item) => {
-    var { image, title, price, ratingStar } = item;
+    var { image, title, price, ratingStar } = item
     return (
         `<div class='box'>
             <div class ='img-box'>
@@ -64,8 +64,8 @@ document.getElementById('root').innerHTML = product.map((item) => {
                 <button onclick='addtocart(${i++})'>Add to cart</button>
             </div>
         </div>`
-    );
-}).join('');
+    )
+}).join('')
 
 // var cart = []
 
@@ -80,30 +80,27 @@ document.getElementById('root').innerHTML = product.map((item) => {
 // }
 
 
-var cart = [];
+var cart = []
 
 function addtocart(a) {
-    cart.push({ ...product[a] });
-    localStorage.setItem('cart', JSON.stringify(cart));
-    displaycart();
+    cart.push({ ...product[a] })
+    displaycart()
 }
 
 function delElement(a) {
-    cart.splice(a, 1);
-    localStorage.setItem('cart', JSON.stringify(cart));
-    displaycart();
+    cart.splice(a, 1)
+    displaycart()
 }
 
 
 function displaycart(a) {
     let j = 0, total = 0
-    document.getElementById("count").innerHTML = cart.length;
     if (cart.length == 0) {
-        document.getElementById('cartItem').innerHTML = "your cart is e mpty";
+        document.getElementById('cartItem').innerHTML = "your cart is e mpty"
         document.getElementById('total').innerHTML = "$ " + 0 + ".00"
     } else
         document.getElementById('cartItem').innerHTML = cart.map((item) => {
-            var { image, title, price } = item;
+            var { image, title, price } = item
             total = total + price
             document.getElementById("total").innerHTML = "$ " + total + '.00'
             return (
@@ -111,31 +108,31 @@ function displaycart(a) {
                 <div class= 'row-img'>
                     <img class= 'rowimg' src = ${image}>
                 </div>
-                <p style = 'font-size: 12px;'>${title}</p>
-                <h2 style= 'font-size:15px;'>$ ${price}.00</h2>` +
+                <p style = 'font-size: 12px'>${title}</p>
+                <h2 style= 'font-size:15px'>$ ${price}.00</h2>` +
                 "<i class='fa-solid fa-trash' onclick='delElement(" + (j++) + ")'></i></div>"
             )
         }).join('')
 }
 
 const applyFilters = () => {
-    const minPrice = parseFloat(document.getElementById("minPrice").value) || 0;
-    const maxPrice = parseFloat(document.getElementById("maxPrice").value) || Number.MAX_VALUE;
-    const minRating = parseFloat(document.getElementById("rattingInput").value) || 0;
-    const searchValue = document.getElementById("searchInput").value.toLowerCase();
+    const minPrice = parseFloat(document.getElementById("minPrice").value) || 0
+    const maxPrice = parseFloat(document.getElementById("maxPrice").value) || Number.MAX_VALUE
+    const minRating = parseFloat(document.getElementById("rattingInput").value) || 0
+    const searchValue = document.getElementById("searchInput").value.toLowerCase()
 
-    const itemList = document.getElementById("root");
+    const itemList = document.getElementById("root")
 
     const filteredProducts = product.filter(item => {
-        const meetsPriceFilter = item.price >= minPrice && item.price <= maxPrice;
-        const meetsRatingFilter = item.ratingStar >= minRating;
-        const meetsSearchFilter = item.title.toLowerCase().includes(searchValue);
+        const meetsPriceFilter = item.price >= minPrice && item.price <= maxPrice
+        const meetsRatingFilter = item.ratingStar == minRating
+        const meetsSearchFilter = item.title.toLowerCase().includes(searchValue)
 
-        return meetsPriceFilter && meetsRatingFilter && meetsSearchFilter;
-    });
+        return meetsPriceFilter && meetsRatingFilter && meetsSearchFilter
+    })
 
     const filteredHTML = filteredProducts.map(item => {
-        const { image, title, price, ratingStar } = item;
+        const { image, title, price, ratingStar } = item
         return `
         <div class='box'>
             <div class ='img-box'>
@@ -148,34 +145,34 @@ const applyFilters = () => {
                 <button onclick='addtocart(${item.id})'>Add to cart</button>
             </div>
         </div>
-    `;
-    }).join('');
+    `
+    }).join('')
 
-    itemList.innerHTML = filteredHTML;
+    itemList.innerHTML = filteredHTML
 }
 
 
 
 // const applyFilters = () => {
-//     const minPrice = parseFloat(document.getElementById("minPrice").value) || 0;
-//     const maxPrice = parseFloat(document.getElementById("maxPrice").value) || Number.MAX_VALUE;
-//     const minRating = parseFloat(document.getElementById("rattingInput").value) || 0;
-//     const searchValue = document.getElementById("searchInput").value.toLowerCase();
+//     const minPrice = parseFloat(document.getElementById("minPrice").value) || 0
+//     const maxPrice = parseFloat(document.getElementById("maxPrice").value) || Number.MAX_VALUE
+//     const minRating = parseFloat(document.getElementById("rattingInput").value) || 0
+//     const searchValue = document.getElementById("searchInput").value.toLowerCase()
 
-//     const itemList = document.getElementById("root");
+//     const itemList = document.getElementById("root")
 
 //     const filteredProducts = product.filter(item => {
-//         const meetsPriceFilter = item.price >= minPrice && item.price <= maxPrice;
-//         const meetsRatingFilter = item.ratingStar >= minRating;
-//         const meetsSearchFilter = item.title.toLowerCase().includes(searchValue);
+//         const meetsPriceFilter = item.price >= minPrice && item.price <= maxPrice
+//         const meetsRatingFilter = item.ratingStar >= minRating
+//         const meetsSearchFilter = item.title.toLowerCase().includes(searchValue)
 
-//         return meetsPriceFilter && meetsRatingFilter && meetsSearchFilter;
-//     });
+//         return meetsPriceFilter && meetsRatingFilter && meetsSearchFilter
+//     })
 
-//     itemList.innerHTML = '';
+//     itemList.innerHTML = ''
 
 //     itemList.innerHTML = filteredProducts.map(item => {
-//         const { image, title, price, ratingStar } = item;
+//         const { image, title, price, ratingStar } = item
 //         return `
 //         <div class='box'>
 //             <div class ='img-box'>
@@ -188,6 +185,6 @@ const applyFilters = () => {
 //                 <button onclick='addtocart(${item.id})'>Add to cart</button>
 //             </div>
 //         </div>
-//     `;
-//     }).join('');
+//     `
+//     }).join('')
 // }
